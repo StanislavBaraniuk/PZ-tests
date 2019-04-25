045ef02f0e5f4754f861c33717a81d5cacd453a9
stanislaw <stanislavbaraniuk@gmail.com>
2019-04-25 15:30:34 +0300

    Load

---
+++ src/pz/PZ.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pz;
+/**
+ *
+ * Пусковий файл програми
+ */
+public class PZ {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainFrame m = new MainFrame();
+        m.setVisible(true);
+    }
+    
+}
---
+++ src/pz/Test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pz;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * Клас для збереження тестових завдань
+ */
+public class Test {
+    public String tile;
+    public ArrayList<String> tests = new ArrayList();
+    public int corect;
+}
